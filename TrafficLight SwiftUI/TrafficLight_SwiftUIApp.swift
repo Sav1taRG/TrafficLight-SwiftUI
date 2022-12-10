fb487021f7d3eac1ab93c6fb614a70b56ecc7d4f
Sav1taRG <52829649+Sav1taRG@users.noreply.github.com>
2022-12-10 12:34:22 +0200

    Initial Commit

---
+++ TrafficLight SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight SwiftUI
+//
+//  Created by Roman Golubinko on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
